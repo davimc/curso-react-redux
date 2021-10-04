59499e8b93ce7c86c5ecc54c507e5850c04ddf5d
Davi <davimatosc@hotmail.com>
2021-10-03 23:57:13 -0300

    UseState: parte #01

--- hooks/src/views/examples/UseState.jsx
+++ hooks/src/views/examples/UseState.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseState = (props) => {
+    let [value, setValue] = useState(0);
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+            <SectionTitle title="exercício #01" />
+            <div className="center">
+                <span className="text">{value}</span>
+                <div>
+                    <button className="btn" onClick={() => setValue(value++)}>+</button>
+                    <button className="btn" onClick={() => setValue(value--)}>-</button>
+                </div>
+            </div>
+            <SectionTitle title="exercício #02"/>
+            
         </div>
     )
 }
